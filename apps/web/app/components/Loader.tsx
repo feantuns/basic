00d06047948449cb117aa37eed0294a2319acac7
Felipe Antunes <felipeantunes542@gmail.com>
2025-10-05 10:14:48 -0300

    trying out different ease options

--- apps/web/app/components/Loader.tsx
+++ apps/web/app/components/Loader.tsx
@@ -3,6 +3,14 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 
+const easings: Record<string, any[]> = {
+  power: [0.83, 0, 0.17, 1], // bad
+  luxury: [0.65, 0, 0.35, 1], // good
+  sharp: [0.85, 0, 0.15, 1],
+  expo: [0.9, 0.1, 0.1, 0.9], // bad
+  soft: [0.77, 0, 0.175, 1],
+};
+
 export default function Loader() {
   const [loading, setLoading] = useState(true);
 
@@ -14,7 +22,7 @@ export default function Loader() {
       style={{ backgroundColor: "#f4f4f4ff" }}
       initial={{ y: 0 }}
       animate={{ y: "-100%" }}
-      transition={{ duration: 1, ease: [0.77, 0, 0.175, 1], delay: 1.5 }}
+      transition={{ duration: 1, ease: easings.luxury, delay: 1.3 }}
       onAnimationComplete={() => setLoading(false)}
     >
       <h1 className="text-black text-8xl font-bold">
@@ -25,7 +33,7 @@ export default function Loader() {
         style={{ backgroundColor: "#f4f4f4ff" }}
         initial={{ y: "100%" }}
         animate={{ y: "0" }}
-        transition={{ duration: 2, ease: [0.77, 0, 0.175, 1], delay: 0.5 }}
+        transition={{ duration: 2.2, ease: easings.luxury, delay: 0.3 }}
       ></motion.div>
     </motion.div>
   );
