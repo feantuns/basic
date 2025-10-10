4099eba2ba7157a7f12a0e16ae22eecf44794ab2
Felipe Antunes <felipeantunes542@gmail.com>
2025-10-10 07:06:37 -0300

    removing loading null for finishing animation correctly

--- apps/web/app/components/Loader.tsx
+++ apps/web/app/components/Loader.tsx
@@ -3,18 +3,8 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 
-const easings: Record<string, any[]> = {
-  power: [0.83, 0, 0.17, 1], // bad
-  luxury: [0.65, 0, 0.35, 1], // good
-  sharp: [0.85, 0, 0.15, 1],
-  expo: [0.9, 0.1, 0.1, 0.9], // bad
-  soft: [0.77, 0, 0.175, 1],
-};
-
 export default function Loader() {
   const [loading, setLoading] = useState(true);
-
-  if (!loading) return null;
 
   return (
     <motion.div
