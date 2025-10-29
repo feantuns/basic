9dc6602f324e12a2398f55970cb14d8ff6f477df
Felipe Antunes <felipeantunes542@gmail.com>
2025-10-29 07:31:16 -0300

    start header

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Loader from "./components/Loader";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <Loader />
+      <Header />
       <main>
         <Hero />
         <section>
