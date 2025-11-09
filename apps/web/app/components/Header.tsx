f0afcb7274bf376430330496ee6021aaeb3de424
Felipe Antunes <felipeantunes542@gmail.com>
2025-11-09 08:50:55 -0300

    correct padding header

--- apps/web/app/components/Header.tsx
+++ apps/web/app/components/Header.tsx
@@ -3,7 +3,7 @@ import HeaderLogo from "./HeaderLogo";
 export default function Header() {
   return (
     <header className="fixed z-51 top-0 w-screen">
-      <div className="px-20 py-10 flex items-center justify-between">
+      <div className="px-20 py-13 flex items-center justify-between">
         <HeaderLogo />
       </div>
     </header>
