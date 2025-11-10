85baa11640945c7aeca9401b4f10e009a6fbf419
Felipe Antunes <felipeantunes542@gmail.com>
2025-11-10 07:23:07 -0300

    start creating nav links

--- apps/web/app/components/Header.tsx
+++ apps/web/app/components/Header.tsx
@@ -5,6 +5,28 @@ export default function Header() {
     <header className="fixed z-51 top-0 w-screen">
       <div className="px-20 py-13 flex items-center justify-between">
         <HeaderLogo />
+        <nav>
+          <ul className="flex gap-16 uppercase text-white text-sm">
+            <li>
+              <a href="/work">Work</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/news">News</a>
+            </li>
+            <li>
+              <a href="/thinking">Thinking</a>
+            </li>
+            <li>
+              <a href="/careers">Careers</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
