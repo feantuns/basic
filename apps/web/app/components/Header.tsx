f8bc9ba6f1e0641277d4352e390e825136baefa0
Felipe Antunes <felipeantunes542@gmail.com>
2025-11-28 07:30:05 -0300

    open full screen menu

--- apps/web/app/components/Header.tsx
+++ apps/web/app/components/Header.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { AnimatedLink } from "./AnimatedLink";
 import HeaderLogo from "./HeaderLogo";
 import { MenuButton } from "./MenuButton";
+import { FullScreenMenu } from "./FullScreenMenu";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,45 +20,50 @@ export default function Header() {
   }, [isMenuOpen]);
 
   return (
-    <header className="fixed z-51 top-0 w-screen">
-      <div className="px-20 py-13 flex items-center justify-between">
-        <HeaderLogo />
-        <nav className="mr-[9rem]">
-          <ul className="flex gap-16 uppercase text-white text-sm">
-            <li>
-              <AnimatedLink href="https://www.basicagency.com/services">
-                Work
-              </AnimatedLink>
-            </li>
-            <li>
-              <AnimatedLink href="https://www.basicagency.com/about">
-                About
-              </AnimatedLink>
-            </li>
-            <li>
-              <AnimatedLink href="https://www.basicagency.com/blog">
-                News
-              </AnimatedLink>
-            </li>
-            <li>
-              <AnimatedLink href="https://www.basicagency.com/thinking">
-                Thinking
-              </AnimatedLink>
-            </li>
-            <li>
-              <AnimatedLink href="https://www.basicagency.com/careers">
-                Careers
-              </AnimatedLink>
-            </li>
-            <li>
-              <AnimatedLink href="https://www.basicagency.com/contact">
-                Contact
-              </AnimatedLink>
-            </li>
-          </ul>
-        </nav>
-        <MenuButton handleClick={() => console.log("open menu")} />
-      </div>
-    </header>
+    <>
+      <header className="fixed z-51 top-0 w-screen">
+        <div className="px-20 py-13 flex items-center justify-between">
+          <HeaderLogo />
+          <nav className="mr-[9rem]">
+            <ul className="flex gap-16 uppercase text-white text-sm">
+              <li>
+                <AnimatedLink href="https://www.basicagency.com/services">
+                  Work
+                </AnimatedLink>
+              </li>
+              <li>
+                <AnimatedLink href="https://www.basicagency.com/about">
+                  About
+                </AnimatedLink>
+              </li>
+              <li>
+                <AnimatedLink href="https://www.basicagency.com/blog">
+                  News
+                </AnimatedLink>
+              </li>
+              <li>
+                <AnimatedLink href="https://www.basicagency.com/thinking">
+                  Thinking
+                </AnimatedLink>
+              </li>
+              <li>
+                <AnimatedLink href="https://www.basicagency.com/careers">
+                  Careers
+                </AnimatedLink>
+              </li>
+              <li>
+                <AnimatedLink href="https://www.basicagency.com/contact">
+                  Contact
+                </AnimatedLink>
+              </li>
+            </ul>
+          </nav>
+          <MenuButton handleClick={() => setIsMenuOpen(true)} />
+        </div>
+      </header>
+
+      {/* The Full Screen Menu Overlay */}
+      <FullScreenMenu isOpen={isMenuOpen} />
+    </>
   );
 }
