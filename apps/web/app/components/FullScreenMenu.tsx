e74d6cee1cff992ca539d47f03ce4a7dd150a567
Felipe Antunes <felipeantunes542@gmail.com>
2025-12-15 06:43:34 -0300

    add title and continue full screen menu content

--- apps/web/app/components/FullScreenMenu.tsx
+++ apps/web/app/components/FullScreenMenu.tsx
@@ -21,6 +21,23 @@ export const FullScreenMenu = ({ isOpen }) => {
   };
 
   const menuVariants: any = {
+    closed: {
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+        ease: [0.76, 0, 0.24, 1], // Custom bezier for smooth "luxury" feel
+      },
+    },
+    open: {
+      opacity: 1,
+      transition: {
+        duration: 0.8,
+        ease: [0.76, 0, 0.24, 1],
+      },
+    },
+  };
+
+  const menuSlideVariants: any = {
     initial: {
       x: "100%",
     },
@@ -63,7 +80,7 @@ export const FullScreenMenu = ({ isOpen }) => {
             className="fixed inset-0 bg-[#1C1C1C] text-[#F4F4F4] z-53 flex flex-col justify-between h-screen w-screen px-5 py-8 md:px-20 md:py-12"
           >
             <div className="flex flex-col w-full h-full">
-              <div className="flex flex-row leading-[110%] text-pink-100 uppercase text-sm gap-12 items-start">
+              <div className="flex flex-row leading-[120%] text-rose-200 uppercase text-sm gap-12 items-start">
                 <div className="flex flex-row gap-8 items-start">
                   <p>●</p>
                   <p>
@@ -78,6 +95,29 @@ export const FullScreenMenu = ({ isOpen }) => {
                   BASIC/DEPT® brand.
                 </p>
               </div>
+
+              <motion.div
+                variants={menuSlideVariants}
+                initial="closed"
+                animate="open"
+                exit="closed"
+              >
+                <div className="flex flex-row leading-[120%] text-rose-200 uppercase text-sm gap-12 items-start">
+                  <div className="flex flex-row gap-8 items-start">
+                    <p>●</p>
+                    <p>
+                      (5) Internal Works
+                      <br />
+                      ©25 <small>c/o</small> BASIC/DEPT®
+                    </p>
+                  </div>
+
+                  <p>
+                    A collection of internal project and initiatives under the
+                    BASIC/DEPT® brand.
+                  </p>
+                </div>
+              </motion.div>
             </div>
           </motion.div>
         </>
